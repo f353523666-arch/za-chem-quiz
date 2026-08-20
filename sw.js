609b2d82a3c5fb98323a66588a9aa066b212eaf4
f353523666-arch <f353523666@gmail.com>
2026-08-20 12:53:24 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-const C='za-chem-v4',A=["./","./index.html","./manifest.webmanifest","./icon.svg","./data1.js","./data2.js","./data3.js","./extras.js","./app.js"];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))).then(()=>self.skipWaiting()));self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='za-chem-final-v6';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./data1.js','./data2.js','./data3.js','./extras.js','./notes.js','./app.js'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))).then(()=>self.skipWaiting()));
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
